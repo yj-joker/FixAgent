a5f43c512ae87e97648f9a961188fecd24b4367b
a01c02 <2720219284@qq..com>
2026-06-04 20:35:32 +0800

    Ignore manual tests

--- manual_tests/test_manual_graph_ingest.py
+++
@@ -1,7 +0,0 @@
-from services.manual_graph_ingest import classify_error
-
-
-def test_classify_error():
-    assert classify_error(TimeoutError("timeout")) == "failed_retryable"
-    assert classify_error(ConnectionError("refused")) == "failed_retryable"
-    assert classify_error(ValueError("no schema sections")) == "failed_permanent"
